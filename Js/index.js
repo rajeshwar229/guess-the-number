45375bd7d1280f9d47e47a89dc98687706b03fb6
Rajeshwar <rajeshwar229@gmail.com>
2023-09-18 03:05:17 -0400

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     let guessnumber;
     let chance;
     let userinput;
-    let msg;
     let queue = [];
 
     const DOM = {
@@ -17,6 +16,7 @@ $(document).ready(function () {
         allButtons: $("button"),
         levelSelect: $("#levelSelect"),
         volumeControls: $(".volume-controls"),
+        bgMusic : $("#background-music"),
         soundEffects: $("#sound-effect"),
         playForm: $('.play-form'),
         levelInput: $(".level-input"),
@@ -26,18 +26,16 @@ $(document).ready(function () {
             return $(`audio[data-link=${dataset}]`);
         },
         hearts: function (chance) {
-            return $(`.heart[data-chance=${chance}]`);
-        }
+            let visibleHearts = $(`.heart:not(.one-chance-less):visible`);
+            return $(`.heart[data-chance=${visibleHearts.length-1}]:visible`);
+        },
+        heartLevels : function(chance1, chance2){
+            return $(`.heart[data-level=${chance1}], .heart[data-level=${chance2}]`)
+        },
+        guessnumError: function(){
+            return $('#guessnum-error');
+        } 
     }
-
-    DOM.playBtn.click(function () {
-        guessnumber = Number.parseInt(Math.ceil(Math.random() * 10));
-        chance = 5;
-        hideShow(DOM.playAgain, DOM.chancesLevelInput);
-        DOM.msg.html("");
-        DOM.numberHistory.html("");
-        DOM.heart.removeClass("one-chance-less").addClass("bg-animate");
-    })
 
     //1st parameter should be hide class and 2nd should be showclass
     const hideShow = function (hideClass, showClass) {
@@ -45,53 +43,18 @@ $(document).ready(function () {
         $(showClass).removeClass('d-none');
     }
 
-    DOM.allButtons.on("click", function () {
-        $("#background-music")[0].play();
-        if (this.dataset.parent && this.dataset.show) {
-            hideShow(`.${this.dataset.parent}`, `.${this.dataset.show}`);
-        }
-    })
-
-    DOM.levelSelect.on('change', function () {
-        DOM.guessNumber.attr('max', this.value);
-        DOM.guessNumber.data('level-check', Number.parseInt(this.value));
-    });
-
-
     // To show the numbers entered till now by the user
-    function numHistory(guessnumber) {
+    const numHistory = function(guess, message) {
         const enterednumbers = document.createElement("li");
-        if (guessnumber) {
-            enterednumbers.innerHTML = `<span class="text-primary fw-bold">Correct number was: ${guessnumber}</span>`;
+        if (guess) {
+            enterednumbers.innerHTML = `<span class="text-primary font-weight-bold">Correct number was: ${guessnumber}</span>`;
         }
         else {
-            enterednumbers.textContent = `Entered number was: ${userinput}`;
+            enterednumbers.textContent = message;
         }
         document.querySelector(".number-history").appendChild(enterednumbers)
         queue.push(userinput);
     }
-
-    DOM.volumeControls.on('click', function () {
-        DOM.audioControl(this.dataset.link).each(function () {
-            if (this.dataset.status === 'on') {
-                this.muted = true;
-                this.dataset.status = 'off';
-            }
-            else {
-                this.muted = false;
-                this.dataset.status = 'on';
-            }
-        });
-        if (this.dataset.status === 'on') {
-            $($(this)[0]).attr('src', this.dataset.off);
-            this.dataset.status = 'off';
-        }
-        else {
-            $($(this)[0]).attr('src', this.dataset.on);
-            this.dataset.status = 'on';
-        }
-
-    })
 
     const gameSound = function (wrong) {
         if (wrong === true) {
@@ -117,67 +80,125 @@ $(document).ready(function () {
         }
     }
 
+    const playBtnClick = function () {
+        guessnumber = Number.parseInt(Math.ceil(Math.random() * 10));
+        $('.correct-num').html(guessnumber);
+        chance = 3;
+        queue = [];
+        hideShow(DOM.playAgain, DOM.chancesLevelInput);
+        DOM.msg.html("");
+        DOM.numberHistory.html("");
+        DOM.heart.removeClass("one-chance-less").addClass("bg-animate");
+        DOM.guessNumber.removeClass('border border-danger error');
+        DOM.guessnumError().hide();
+        DOM.levelSelect.prop('selectedIndex',0);
+        DOM.heartLevels(4,5).addClass('d-none');
+    }
+
+    const allButtonsClick = function () {
+        DOM.bgMusic[0].play();
+        DOM.bgMusic[0].volume = 0.01;
+        if (this.dataset.parent && this.dataset.show) {
+            hideShow(`.${this.dataset.parent}`, `.${this.dataset.show}`);
+        }
+    }
+    
+    const levelSelectChange = function () {
+        DOM.guessNumber.attr('max', this.value);
+        DOM.guessNumber.data('level-check', Number.parseInt(this.value));
+        chance = +$(this).find('option:selected').attr('data-chance');
+        DOM.heartLevels(chance, chance+1).addClass('d-none');
+        DOM.heartLevels(chance, chance-1).removeClass('d-none');
+        guessnumber = Number.parseInt(Math.ceil(Math.random() * this.value));
+        $('.correct-num').html(guessnumber);
+    }
+
+    const volumeControlsClick =  function () {
+        DOM.audioControl(this.dataset.link).each(function () {
+            if (this.dataset.status === 'on') {
+                this.muted = true;
+                this.dataset.status = 'off';
+            }
+            else {
+                this.muted = false;
+                this.dataset.status = 'on';
+            }
+        });
+        if (this.dataset.status === 'on') {
+            $($(this)[0]).attr('src', this.dataset.off);
+            this.dataset.status = 'off';
+        }
+        else {
+            $($(this)[0]).attr('src', this.dataset.on);
+            this.dataset.status = 'on';
+        }
+
+    };
+
     // Unmute the volume when game is active
-    $(window).focus(function () {
+    const windowFocus = function () {
         DOM.audioControl('music').each(function () {
             if (this.dataset.status === 'on') {
                 this.muted = false;
             }
         });
-    });
+    };
+
     // Mute the volume when game is inactive
-    $(window).blur(function () {
+    const windowBlur = function () {
         DOM.audioControl('music').each(function () {
             this.muted = true;
         });
-    });
+    };
 
-    $(document).on("keypress", function (event) {
+    const documentKeypress = function (event) {
         if (DOM.playForm.valid() && event.key === "Enter") {
             event.preventDefault();
 
-            // Number entered by user
-            userinput = DOM.guessNumber.val();
-
-            // smaller number
-            if (userinput < guessnumber) {
-                DOM.msg.html("Sorry! The number entered is smaller than the correct number");
+            const greaterOrSmaller = function(msg){
+                DOM.msg.html(msg);
                 gameSound(true);
                 chance--;
                 DOM.hearts(chance).addClass("one-chance-less");
                 DOM.hearts(chance).removeClass("bg-animate");
-                numHistory();
+                numHistory(false, msg);
             }
+            // Number entered by user, Math.floor to remove if float number entered
+            userinput = Math.floor(DOM.guessNumber.val());
+            
+            // Check if number already not entered
+            if(queue.indexOf(userinput) < 0){
+            // smaller number
+                if (userinput < guessnumber) {
+                    greaterOrSmaller(`Sorry! The number ${userinput} is smaller than the correct number`);
+                }
 
-            // greater number
-            else if (userinput > guessnumber) {
-                DOM.msg.html("Sorry! The number entered is greater than the correct number");
-                gameSound(true);
-                 chance--;
-                 DOM.hearts(chance).addClass("one-chance-less");
-                 DOM.hearts(chance).removeClass("bg-animate");
-                 numHistory();
+                // greater number
+                else if (userinput > guessnumber) {
+                    greaterOrSmaller(`Sorry! The number ${userinput} is greater than the correct number`);
+                }
+            
+                //When no chances left
+                if (!chance) {
+                    DOM.msg.html("<img src='images/fail-img.gif' class='soryimg img-fluid'>");
+                    hideShow(DOM.levelInput, DOM.playAgain);
+                    gameSound("outOfMoves");
+                    numHistory(guessnumber);
+                }
+
+                //Congrats
+                if (userinput == guessnumber) {
+                    DOM.msg.html("<img src='images/success-img.gif' class='correct img-fluid' width='450'>");
+                    hideShow(DOM.chancesLevelInput, DOM.playAgain);
+                    gameSound("win");
+                    numHistory(guessnumber);
+                }
+
             }
-
-           
-           
-          
-            if (!chance) {
-                DOM.msg.html("<img src='images/fail-img.gif' class='soryimg img-fluid'>");
-                hideShow(DOM.levelInput, DOM.playAgain);
-                gameSound("outOfMoves");
-                numHistory(guessnumber);
+            else{
+                DOM.msg.html(`You have already guessed ${userinput}, Guess another number`);
             }
-
-            //Congrats
-            if (userinput == guessnumber) {
-                DOM.msg.html("<img src='images/success-img.gif' class='correct img-fluid' width='450'>");
-                hideShow(DOM.chancesLevelInput, DOM.playAgain);
-                gameSound("win");
-                numHistory(guessnumber);
-            }
-
-           
+            
             DOM.guessNumber.val('');
         }
 
@@ -185,5 +206,14 @@ $(document).ready(function () {
         else if (DOM.guessNumber.hasClass('error')) {
             gameSound("alert");
         }
-    });
-})
+    };
+
+    //All user events at one place
+    DOM.playBtn.on('click', playBtnClick);
+    DOM.allButtons.on("click", allButtonsClick);
+    DOM.levelSelect.on('change', levelSelectChange);
+    DOM.volumeControls.on('click', volumeControlsClick);
+    $(window).on('focus', windowFocus);
+    $(window).on('blur', windowBlur);
+    $(document).on("keypress", documentKeypress);
+});
